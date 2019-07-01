599964ffad7159b76256b0e5dece0fe094bd7ef8
mpomesar <martipomes@hotmail.com>
2019-07-01 21:53:50 +0200

    Update train.py

--- final-version/train.py
+++ final-version/train.py
@@ -28,35 +28,35 @@ def train(model, learning_rate=1e-4):
     density_train=[]
 
     for epoch in range(EPOCHS):
-    for i,batch in enumerate(training_generator):
-        # Forward pass: Compute predicted y by passing x to the model
-        x = batch[0]
-        y = batch[1]
-        x= x.to(device)
-        y= y.to(device)
+        for i,batch in enumerate(training_generator):
+            # Forward pass: Compute predicted y by passing x to the model
+            x = batch[0]
+            y = batch[1]
+            x= x.to(device)
+            y= y.to(device)
 
-        y_pred = model.train()(x,y,teacher_forcing_ratio=0.5*(1-epoch/EPOCHS)**2)
+            y_pred = model.train()(x,y,teacher_forcing_ratio=0.5*(1-epoch/EPOCHS)**2)
 
-        # Compute and print loss
-        loss = model.focal_loss(y_pred, y, alpha=0.75, gamma=2.0)
-        recall  = model.recall(y_pred, y)
-        precision = model.precision(y_pred, y)
+            # Compute and print loss
+            loss = model.focal_loss(y_pred, y, alpha=0.75, gamma=2.0)
+            recall  = model.recall(y_pred, y)
+            precision = model.precision(y_pred, y)
 
-        # Zero gradients, perform a backward pass, and update the weights.
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            # Zero gradients, perform a backward pass, and update the weights.
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-    loss_train.append(loss.item())
+        loss_train.append(loss.item())
 
-    #how many of the notes on the composition where predicted
-    recall_train.append(recall.item())
+        #how many of the notes on the composition where predicted
+        recall_train.append(recall.item())
 
-    #how many of the notes predicted followed the composition
-    precision_train.appen(precision.item())
+        #how many of the notes predicted followed the composition
+        precision_train.appen(precision.item())
 
-    #represents the density of the pianoroll. Good values for 176 (COD_TYPE=2) 4 voices tend to be arround 0.025
-    density_train.append((y_pred>model.thr).float().mean())
+        #represents the density of the pianoroll. Good values for 176 (COD_TYPE=2) 4 voices tend to be arround 0.025
+        density_train.append((y_pred>model.thr).float().mean())
 
-    print(epoch, loss_train[epoch], recall_train[epoch], precision_train[epoch], density_train[epoch])
+        print(epoch, loss_train[epoch], recall_train[epoch], precision_train[epoch], density_train[epoch])
 
