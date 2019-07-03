8459c202c379a293ac0e5463054533931b87da4f
mpomesar <martipomes@hotmail.com>
2019-07-04 00:38:21 +0200

    Update training.py

--- final-version/training.py
+++ final-version/training.py
@@ -32,7 +32,7 @@ MetaValues
     MIDI_SOURCE = "tempData/midi" #For Bach Choral a
     #MIDI_SOURCE = download_datasets(data_set=1)
 	
-	CASABLANCA=4
+	QNSF=2*2
     
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
